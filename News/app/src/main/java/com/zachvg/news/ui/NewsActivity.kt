8f5e5dc7ee241fb17de2e61ec200b4afe1486181
Zach <zvongrote@gmail.com>
2021-05-18 10:30:44 -0700

    News: setup navigation
    Created the main flow of navigation, and added needed icons and animations

---
+++ News/app/src/main/java/com/zachvg/news/ui/NewsActivity.kt
@@ -0,0 +1,29 @@
+package com.zachvg.news.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.zachvg.news.R
+import com.zachvg.news.databinding.ActivityNewsBinding
+
+class NewsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNewsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Use this code to access the navcontroller in onCreate() when using a FragmentContainerView:
+        // https://stackoverflow.com/a/59275182/9633447
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+}
---
+++ News/app/src/main/java/com/zachvg/news/ui/fragments/SavedNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.zachvg.news.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.zachvg.news.R
+
+class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
+}
